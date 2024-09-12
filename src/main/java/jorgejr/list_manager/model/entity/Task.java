bb6fd4341c8a64d2cbbdf8e7729cf332a5f3600a
Jorge jr <jorgesilvajunior10@gmail.com>
2024-09-12 14:26:57 -0300

    add: entities

---
+++ src/main/java/jorgejr/list_manager/model/entity/Task.java
@@ -0,0 +1,90 @@
+package jorgejr.list_manager.model.entity;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "task")
+public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 255)
+    private String titulo;
+
+    @Column
+    private String descricao;
+
+    @Column(name = "data_criacao", columnDefinition = "TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP")
+    private LocalDateTime dataCriacao;
+
+    @Column(name = "data_conclusao")
+    private LocalDateTime dataConclusao;
+
+    @Column
+    private Boolean concluida = false;
+
+    @ManyToOne
+    @JoinColumn(name = "task_list_id", nullable = false)
+    private TaskList taskList;
+
+    // Getters and Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public LocalDateTime getDataCriacao() {
+        return dataCriacao;
+    }
+
+    public void setDataCriacao(LocalDateTime dataCriacao) {
+        this.dataCriacao = dataCriacao;
+    }
+
+    public LocalDateTime getDataConclusao() {
+        return dataConclusao;
+    }
+
+    public void setDataConclusao(LocalDateTime dataConclusao) {
+        this.dataConclusao = dataConclusao;
+    }
+
+    public Boolean getConcluida() {
+        return concluida;
+    }
+
+    public void setConcluida(Boolean concluida) {
+        this.concluida = concluida;
+    }
+
+    public TaskList getTaskList() {
+        return taskList;
+    }
+
+    public void setTaskList(TaskList taskList) {
+        this.taskList = taskList;
+    }
+}
---
+++ src/main/java/jorgejr/list_manager/model/entity/TaskList.java
@@ -0,0 +1,42 @@
+package jorgejr.list_manager.model.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "task_list")
+public class TaskList {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 255)
+    private String nome;
+
+    @Column
+    private String descricao;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+}
